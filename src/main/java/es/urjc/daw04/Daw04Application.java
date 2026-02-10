2592365c6c9c34c656c345cdf186d15ac3ec9eac
Álvaro <alvarofuenteg@gmail.com>
2026-02-10 10:13:28 +0100

    Practice 1 Maven v 0.3 Created and adapted everything to Maven

---
+++ src/main/java/es/urjc/daw04/Daw04Application.java
@@ -0,0 +1,13 @@
+package es.urjc.daw04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daw04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daw04Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/urjc/daw04/Daw04ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjc.daw04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daw04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
