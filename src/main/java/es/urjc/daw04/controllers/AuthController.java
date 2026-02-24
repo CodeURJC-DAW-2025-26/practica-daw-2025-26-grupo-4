f80e99ecbf8657381c426037423efd8892dc73e6
“Turo” <arturo.vinuesadcg@gmail.com>
2026-02-24 13:14:04 +0100

    Creacion de la base de seguridad en los usuarios guardando sus datos en la base de datos.

--- src/main/java/es/urjc/daw04/controllers/AuthController.java
+++ src/main/java/es/urjc/daw04/controllers/AuthController.java
@@ -4,11 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
-import es.urjc.daw04.services.UserService;
-
 import es.urjc.daw04.service.CartService;
 
 @Controller
@@ -17,22 +13,19 @@ public class AuthController {
     @Autowired
     private CartService cartService;
 
-    @Autowired
-    private UserService userService;
-
     @GetMapping("/login")
     public String login() {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String loginPost(@RequestParam String username, @RequestParam String password, Model model) {
-        if (userService.validateUser(username, password)) {
-            return "redirect:/";
-        } else {
-            model.addAttribute("loginError", true);
-            return "login";
-        }
+    @GetMapping("/loginerror")
+    public String loginerror() {
+        return "loginerror";
+    }
+
+    @GetMapping("/private")
+    public String privatePage() {
+        return "private";
     }
 
     @GetMapping("/user")
