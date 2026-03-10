de8a1f8bae73c5864bcb307a978752cc80a591d2
Álvaro <alvarofuenteg@gmail.com>
2026-03-10 09:40:28 +0100

    Practice 2 v 1.1 Completed Security (the requestMatchers URLs are wrong still)

--- src/main/java/es/urjc/daw04/security/WebSecurityConfig.java
+++ src/main/java/es/urjc/daw04/security/WebSecurityConfig.java
@@ -3,18 +3,39 @@ package es.urjc.daw04.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+import es.urjc.daw04.security.jwt.JwtRequestFilter;
+import es.urjc.daw04.security.jwt.JwtTokenProvider;
+import es.urjc.daw04.security.jwt.UnauthorizedHandlerJwt;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UnauthorizedHandlerJwt unauthorizedHandlerJwt;
+
+    @Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
 
     @Autowired
     public RepositoryUserDetailsService userDetailService;
@@ -33,6 +54,53 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    @Order(1)
+	public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+
+		http.authenticationProvider(authenticationProvider());
+
+		http
+				.securityMatcher("/api/**")
+				.exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+
+		http
+				.authorizeHttpRequests(authorize -> authorize
+						// PRIVATE ENDPOINTS
+						// Images
+						.requestMatchers(HttpMethod.PUT, "/api/images/*/media").hasRole("USER")
+						.requestMatchers(HttpMethod.DELETE, "/api/books/*/images/*").hasRole("USER")
+						// Books
+						.requestMatchers(HttpMethod.POST, "/api/books/**").hasRole("USER")
+						.requestMatchers(HttpMethod.PUT, "/api/books/**").hasRole("USER")
+						.requestMatchers(HttpMethod.DELETE, "/api/books/**").hasRole("ADMIN")
+						// Shops
+						.requestMatchers(HttpMethod.PUT, "/api/shops/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.PUT, "/api/shops/**").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.DELETE, "/api/shops/**").hasRole("ADMIN")
+						// PUBLIC ENDPOINTS
+						.anyRequest().permitAll());
+
+		// Disable Form login Authentication
+		http.formLogin(formLogin -> formLogin.disable());
+
+		// Disable CSRF protection (it is difficult to implement in REST APIs)
+		http.csrf(csrf -> csrf.disable());
+
+		// Disable Basic Authentication
+		http.httpBasic(httpBasic -> httpBasic.disable());
+
+		// Stateless session
+		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		// Add JWT Token filter
+		http.addFilterBefore(new JwtRequestFilter(userDetailService, jwtTokenProvider),
+				UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authenticationProvider(authenticationProvider());
