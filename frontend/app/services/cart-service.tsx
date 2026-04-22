f69cf5afb62ab107cedb345ae60642af7fea87ab
DarioGarciaGomez <d.garciago.2023@alumnos.urjc.es>
2026-04-22 11:30:48 +0200

    Practice 3 v2.10 cart service implemented

---
+++ frontend/app/services/cart-service.tsx
@@ -0,0 +1,75 @@
+import type { CartDTO } from "~/api/dtos";
+
+const API_URL = "/api/v1/cart";
+
+export const cartService = {
+  async getCart(): Promise<CartDTO | null> {
+    const response = await fetch(`${API_URL}/`, {
+      method: "GET",
+      credentials: "omit",
+    });
+
+    if (response.status === 401) {
+      return null;
+    }
+
+    if (!response.ok) throw new Error("Error al buscar el carrito");
+    return response.json();
+  },
+
+  async addItem(productId: number, quantity: number = 1) {
+    const response = await fetch(
+      `${API_URL}/items/${productId}?quantity=${quantity}`,
+      {
+        method: "POST",
+        credentials: "omit",
+      },
+    );
+
+    if (response.status === 401) {
+      throw new Error("Debes haber iniciado sesión como usuario.");
+    }
+
+    if (!response.ok) throw new Error("Error al añadir al carrito");
+    return response.json();
+  },
+
+  async removeItem(productId: number) {
+    const response = await fetch(`${API_URL}/items/${productId}`, {
+      method: "DELETE",
+      credentials: "omit",
+    });
+
+    if (response.status === 401) {
+      throw new Error("Debes haber iniciado sesión como usuario.");
+    }
+
+    if (!response.ok) throw new Error("Error al eliminar del carrito");
+    return response.json();
+  },
+
+  async clearCart() {
+    const response = await fetch(API_URL, {
+      method: "DELETE",
+      credentials: "omit",
+    });
+
+    if (response.status === 401) {
+      throw new Error("Debes haber iniciado sesión como usuario.");
+    }
+
+    if (!response.ok) throw new Error("Error al eliminar el carrito");
+  },
+
+  async checkout() {
+    const response = await fetch(`${API_URL}/checkout`, {
+      method: "POST",
+      credentials: "omit",
+    });
+    if (response.status === 401) {
+      throw new Error("Debes haber iniciado sesión como usuario.");
+    }
+
+    if (!response.ok) throw new Error("Error al procesar el carrito");
+  },
+};
